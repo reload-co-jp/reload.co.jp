9500ec76cb82b61387d99e7371367d546f436c78
kixixixixi <kixixixixi@gmail.com>
2023-03-27 03:06:11 +0900

    Add google analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,15 @@
+import partytown from "@astrojs/partytown"
 import { defineConfig } from "astro/config"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://reload.co.jp",
+  integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 })
