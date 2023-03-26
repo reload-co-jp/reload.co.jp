8c1af8283c2d4daec013424ab8ee7cb7c4fc2037
kixixixixi <kixixixixi@gmail.com>
2023-03-27 03:51:02 +0900

    Add astro compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import partytown from "@astrojs/partytown"
 import { defineConfig } from "astro/config"
+import compress from "astro-compress"
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://reload.co.jp",
   integrations: [
+    compress(),
     partytown({
       // Adds dataLayer.push as a forwarding-event.
       config: {
