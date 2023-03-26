5a50f671815e792309c35d0ff64aced269d5c715
kixixixixi <kixixixixi@gmail.com>
2023-03-27 03:39:35 +0900

    Format stylelint config

--- stylelint.config.cjs
+++ stylelint.config.cjs
@@ -1,26 +1,25 @@
 module.exports = {
   // add your custom config here
   // https://stylelint.io/user-guide/configuration
-  customSyntax: 'postcss-html',
+  customSyntax: "postcss-html",
   plugins: [
-    'stylelint-scss',
-    'stylelint-declaration-block-no-ignored-properties',
-    'stylelint-no-unsupported-browser-features',
-    'stylelint-prettier',
+    "stylelint-scss",
+    "stylelint-declaration-block-no-ignored-properties",
+    "stylelint-no-unsupported-browser-features",
+    "stylelint-prettier",
   ],
-  extends: ['stylelint-config-recess-order'],
+  extends: ["stylelint-config-recess-order"],
   rules: {
-    'prettier/prettier': true,
-    'plugin/declaration-block-no-ignored-properties': true,
-    'plugin/no-unsupported-browser-features': [
+    "prettier/prettier": true,
+    "plugin/declaration-block-no-ignored-properties": true,
+    "plugin/no-unsupported-browser-features": [
       true,
       {
-        severity: 'warning',
+        severity: "warning",
       },
     ],
-    'font-family-no-missing-generic-family-keyword': true,
-    'declaration-block-no-shorthand-property-overrides': true,
-    'selector-pseudo-element-colon-notation': 'double',
+    "font-family-no-missing-generic-family-keyword": true,
+    "declaration-block-no-shorthand-property-overrides": true,
+    "selector-pseudo-element-colon-notation": "double",
   },
 }
-
